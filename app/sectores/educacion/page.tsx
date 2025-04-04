8283c54029d13b457da439c2eb4bd09b0f11496f
Diego Barboza <usuario@ejemplo.com>
2025-04-04 10:53:16 -0400

    update-sectoresss

--- app/sectores/educacion/page.tsx
+++ app/sectores/educacion/page.tsx
@@ -269,6 +269,7 @@ export default function Educacion() {
               </div>
             </div>
             
+            
             {/* Beneficios */}
             <div className="lg:col-span-7 order-1 lg:order-2">
               <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 md:gap-6">
